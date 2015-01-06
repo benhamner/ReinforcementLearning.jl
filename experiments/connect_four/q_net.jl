8eab11d668da401fd893a537255038644f73cc02
Ben Hamner <ben@benhamner.com>
2015-01-05 20:30:48 -0500

    removed duplicate and forked file

--- experiments/connect_four/q_net.jl
+++ experiments/connect_four/q_net.jl
@@ -1,15 +1,22 @@
 using ReinforcementLearning
 
-@time q_net, q_net_player = train_q_net_player(play_connect_four_track_state,
-                                               84,
-                                               [random_player],
-                                               num_games=100_000,
-                                               hidden_layers=[100,100,20,20])
-@time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, random_player, 1_000)
-println("Q net vs random: ", results_txt)
+for hidden_layers = Vector{Int}[[100, 100, 20], [20], [50], [100], [200]]
+    println("Hidden Layers: ", hidden_layers)
+    @time q_net, q_net_player = train_q_net_player(play_connect_four_track_state,
+                                                   84,
+                                                   [random_player],
+                                                   num_games=100_000,
+                                                   hidden_layers=hidden_layers)
+    @time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, random_player, 2_000)
+    println("Q net vs random: ", results_txt)
+    win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, random_player, 2_000)
+    println("Q net vs random: ", results_txt)
 
-@time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(1), 1_000)
-println("Q net vs ahead1: ", results_txt)
+    @time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(1), 2_000)
+    println("Q net vs ahead1: ", results_txt)
+    win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(1), 2_000)
+    println("Q net vs ahead1: ", results_txt)
 
-@time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(2), 1_000)
-println("Q net vs ahead2: ", results_txt)
+    @time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(2), 200)
+    println("Q net vs ahead2: ", results_txt)
+end
