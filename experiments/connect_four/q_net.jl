9f8fc42b93ff35dbaaedd1cd7a52e4dd46b976a0
Ben Hamner <ben@benhamner.com>
2015-01-07 01:31:49 -0800

    minor updates to experiments

--- experiments/connect_four/q_net.jl
+++ experiments/connect_four/q_net.jl
@@ -1,23 +1,26 @@
 using MachineLearning
 using ReinforcementLearning
 
-for hidden_layers = Vector{Int}[[100, 100, 20], [20], [50], [100], [200]]
-    println("Hidden Layers: ", hidden_layers)
-    @time q_net, q_net_player = train_q_net_player(play_connect_four_track_state,
-                                                   84,
-                                                   [random_player],
-                                                   num_games=100_000,
-                                                   net_options=regression_net_options(hidden_layers=[hidden_layers], regularization_factor=0.0))
-    @time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, random_player, 2_000)
-    println("Q net vs random: ", results_txt)
-    win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, random_player, 2_000)
-    println("Q net vs random: ", results_txt)
+hidden_layers = [100,100]
+learning_rate = 500.0
+num_games     = 500_000
 
-    @time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(1), 2_000)
-    println("Q net vs ahead1: ", results_txt)
-    win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(1), 2_000)
-    println("Q net vs ahead1: ", results_txt)
+println("Hidden Layers: ", hidden_layers)
+println("Learning Rate: ", learning_rate)
+println("Num Games:     ", num_games)
 
-    @time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(2), 200)
-    println("Q net vs ahead2: ", results_txt)
-end
+@time q_net, q_net_player = train_q_net_player(play_connect_four_track_state,
+                                               84,
+                                               [random_player],
+                                               num_games=num_games,
+                                               net_options=regression_net_options(hidden_layers=hidden_layers,
+                                                                                  regularization_factor=0.0,
+                                                                                  learning_rate=learning_rate))
+@time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, random_player, 2_000)
+println("qnet v rand: ", results_txt)
+
+@time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(1), 2_000)
+println("qnet v lh1:  ", results_txt)
+
+@time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_net_player, make_lookahead_player(2), 400)
+println("qnet v lh2:  ", results_txt)
