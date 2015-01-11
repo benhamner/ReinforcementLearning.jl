49b23b2427e63a2e6f09264fb7230822b6ef0104
Ben Hamner <ben@benhamner.com>
2015-01-10 23:09:51 -0800

    minor update

--- experiments/tic_tac_toe/players.jl
+++ experiments/tic_tac_toe/players.jl
@@ -10,8 +10,10 @@ println("Hidden Layers: ", hidden_layers)
 println("Learning Rate: ", learning_rate)
 println("Num Games:     ", num_games)
 
-res  = DataFrame(Name=[], Iteration=[], Score=[], Group=[])
-res2 = DataFrame(Name=[], Iteration=[], WinPercentage=[], DrawPercentage=[], LossPercentage=[])
+vrand  = DataFrame(Name=[], Iteration=[], Score=[], Group=[])
+vrand2 = DataFrame(Name=[], Iteration=[], WinPercentage=[], DrawPercentage=[], LossPercentage=[])
+vperf  = DataFrame(Name=[], Iteration=[], Score=[], Group=[])
+vperf2 = DataFrame(Name=[], Iteration=[], WinPercentage=[], DrawPercentage=[], LossPercentage=[])
 iterations=50
 players = Vector{Function}[[random_player], [random_player, perfect_player], [perfect_player]]
 player_names = ["Rand", "Rand+Perf", "Perf"]
@@ -26,11 +28,19 @@ for i=1:iterations
                                                                                   learning_rate=learning_rate))
         win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_tic_tac_toe_players(q_net_player, random_player, 2_000)
         println("qnet v rand: ", results_txt)
-        res  = vcat(res,  DataFrame(Name=player_names[j], Iteration=i, Score=win_percentage,  Group="Win"))
-        res  = vcat(res,  DataFrame(Name=player_names[j], Iteration=i, Score=draw_percentage, Group="Draw"))
-        res  = vcat(res,  DataFrame(Name=player_names[j], Iteration=i, Score=loss_percentage, Group="Loss"))
-        res2 = vcat(res2, DataFrame(Name=player_names[j], Iteration=i, WinPercentage=win_percentage, DrawPercentage=draw_percentage, LossPercentage=loss_percentage))
+        vrand  = vcat(vrand,  DataFrame(Name=player_names[j], Iteration=i, Score=win_percentage,  Group="Win"))
+        vrand  = vcat(vrand,  DataFrame(Name=player_names[j], Iteration=i, Score=draw_percentage, Group="Draw"))
+        vrand  = vcat(vrand,  DataFrame(Name=player_names[j], Iteration=i, Score=loss_percentage, Group="Loss"))
+        vrand2 = vcat(vrand2, DataFrame(Name=player_names[j], Iteration=i, WinPercentage=win_percentage, DrawPercentage=draw_percentage, LossPercentage=loss_percentage))
+        win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_tic_tac_toe_players(q_net_player, perfect_player, 2_000)
+        println("qnet v perf: ", results_txt)
+        vperf  = vcat(vperf,  DataFrame(Name=player_names[j], Iteration=i, Score=win_percentage,  Group="Win"))
+        vperf  = vcat(vperf,  DataFrame(Name=player_names[j], Iteration=i, Score=draw_percentage, Group="Draw"))
+        vperf  = vcat(vperf,  DataFrame(Name=player_names[j], Iteration=i, Score=loss_percentage, Group="Loss"))
+        vperf2 = vcat(vperf2, DataFrame(Name=player_names[j], Iteration=i, WinPercentage=win_percentage, DrawPercentage=draw_percentage, LossPercentage=loss_percentage))
     end
-    draw(PNG("plots/players.png", 8inch, 6inch), plot(res, x=:Name, y=:Score, color=:Group, Geom.boxplot))
-    draw(PNG("plots/players_scatter.png", 8inch, 6inch), plot(res2, x=:WinPercentage, y=:DrawPercentage, color=:Name, Geom.point))
+    draw(PNG("plots/players_vrand.png", 8inch, 6inch), plot(vrand, x=:Name, y=:Score, color=:Group, Geom.boxplot))
+    draw(PNG("plots/players_vrand_scatter.png", 8inch, 6inch), plot(vrand2, x=:WinPercentage, y=:DrawPercentage, color=:Name, Geom.point))
+    draw(PNG("plots/players_vperf.png", 8inch, 6inch), plot(vperf, x=:Name, y=:Score, color=:Group, Geom.boxplot))
+    draw(PNG("plots/players_vperf_scatter.png", 8inch, 6inch), plot(vperf2, x=:DrawPercentage, y=:LossPercentage, color=:Name, Geom.point))
 end
