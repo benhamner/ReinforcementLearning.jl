dcb654975baa2b164442dda4800e71b4fa89c350
Ben Hamner <ben@benhamner.com>
2015-01-18 17:29:55 -0700

    refactor

--- experiments/connect_four/players.jl
+++ experiments/connect_four/players.jl
@@ -4,7 +4,7 @@ using ReinforcementLearning
 
 hidden_layers = [25]
 learning_rate = 500.0
-num_games     = 50_000
+num_games     = 1_000
 
 println("Hidden Layers: ", hidden_layers)
 println("Learning Rate: ", learning_rate)
@@ -24,7 +24,7 @@ train_functions = vcat([() -> train_q_net_player(play_connect_four_track_state,
                                                                                     regularization_factor=0.0,
                                                                                     learning_rate=learning_rate),
                                                  self_play=play_self[i],
-                                                 alpha=0.5) for i=1:length(base_players)],
+                                                 alpha=0.75) for i=1:length(base_players)],
                        [() -> train_q_net_player(play_connect_four_track_state,
                                                  84,
                                                  base_players[i],
@@ -33,33 +33,28 @@ train_functions = vcat([() -> train_q_net_player(play_connect_four_track_state,
                                                                                     regularization_factor=0.0,
                                                                                     learning_rate=learning_rate),
                                                  self_play=play_self[i],
-                                                 alpha=0.1) for i=1:length(base_players)])
-player_names = vcat(["A0.5"*n for n=base_player_names],["A0.1"*n for n=base_player_names])
+                                                 alpha=0.25) for i=1:length(base_players)])
+player_names = vcat(["A0.75"*n for n=base_player_names],["A0.25"*n for n=base_player_names])
 
-for i=1:iterations
-    println("Iteration: ", i)
-    for j=1:length(train_functions)
-        q, q_player = train_functions[j]()
-        win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_player, random_player, 1_000)
-        res = vcat(res, DataFrame(Name=player_names[j], Opponent="Rand", Iteration=i, WinPercentage=win_percentage, DrawPercentage=draw_percentage, LossPercentage=loss_percentage))
-        println("qnet v rand: ", results_txt)
-        @time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_player, make_lookahead_player(1), 1_000)
-        res = vcat(res, DataFrame(Name=player_names[j], Opponent="LH1", Iteration=i, WinPercentage=win_percentage, DrawPercentage=draw_percentage, LossPercentage=loss_percentage))
-        println("qnet v lh1:  ", results_txt)
-        @time win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_player, make_lookahead_player(2), 1_000)
-        res = vcat(res, DataFrame(Name=player_names[j], Opponent="LH2", Iteration=i, WinPercentage=win_percentage, DrawPercentage=draw_percentage, LossPercentage=loss_percentage))
-        println("qnet v lh2:  ", results_txt)
+evaluation_opponents = [random_player, make_lookahead_player(1), make_lookahead_player(2)]
+evaluation_opponent_names = ["rand", "lh1", "lh2"]
+
+for iteration=1:iterations
+    println("Iteration: ", iteration)
+    for i_train_function=1:length(train_functions)
+        q, q_player = train_functions[i_train_function]()
+        for i_opponent = 1:length(evaluation_opponents)
+            win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_player, evaluation_opponents[i_opponent], 100)
+            opponent_name = evaluation_opponent_names[i_opponent]
+            res = vcat(res, DataFrame(Name=player_names[i_train_function], Opponent=opponent_name, Iteration=iteration, WinPercentage=win_percentage, DrawPercentage=draw_percentage, LossPercentage=loss_percentage))
+            println("qnet v ", opponent_name, ": ", results_txt)
+        end
     end
-    stacked_rand = stack(res[res[:Opponent].=="Rand",:], [:WinPercentage, :DrawPercentage, :LossPercentage])
-    rename!(stacked_rand, [:variable, :value], [:Group, :Score])
-    stacked_lh1  = stack(res[res[:Opponent].=="LH1",:], [:WinPercentage, :DrawPercentage, :LossPercentage])
-    rename!(stacked_lh1, [:variable, :value], [:Group, :Score])
-    stacked_lh2  = stack(res[res[:Opponent].=="LH2",:], [:WinPercentage, :DrawPercentage, :LossPercentage])
-    rename!(stacked_lh2, [:variable, :value], [:Group, :Score])
-    draw(PNG("plots/players_vrand.png", 8inch, 6inch), plot(stacked_rand, x=:Name, y=:Score, color=:Group, Geom.boxplot))
-    draw(PNG("plots/players_vrand_scatter.png", 8inch, 6inch), plot(res[res[:Opponent].=="Rand",:], x=:WinPercentage, y=:DrawPercentage, color=:Name, Geom.point))
-    draw(PNG("plots/players_vlh1.png", 8inch, 6inch), plot(stacked_lh1, x=:Name, y=:Score, color=:Group, Geom.boxplot))
-    draw(PNG("plots/players_vlh1_scatter.png", 8inch, 6inch), plot(res[res[:Opponent].=="LH1",:], x=:DrawPercentage, y=:LossPercentage, color=:Name, Geom.point))
-    draw(PNG("plots/players_vlh2.png", 8inch, 6inch), plot(stacked_lh2, x=:Name, y=:Score, color=:Group, Geom.boxplot))
-    draw(PNG("plots/players_vlh2_scatter.png", 8inch, 6inch), plot(res[res[:Opponent].=="LH2",:], x=:DrawPercentage, y=:LossPercentage, color=:Name, Geom.point))
+    for i_opponent = 1:length(evaluation_opponents)
+        opponent_name = evaluation_opponent_names[i_opponent]
+        stacked = stack(res[res[:Opponent].==opponent_name,:], [:WinPercentage, :DrawPercentage, :LossPercentage])
+        rename!(stacked, [:variable, :value], [:Group, :Score])
+        draw(PNG(@sprintf("plots/players_v%s.png", opponent_name), 8inch, 6inch), plot(stacked, x=:Name, y=:Score, color=:Group, Geom.boxplot))
+        draw(PNG(@sprintf("plots/players_v%s_scatter.png", opponent_name), 8inch, 6inch), plot(res[res[:Opponent].==opponent_name,:], x=:WinPercentage, y=:DrawPercentage, color=:Name, Geom.point))
+    end
 end
