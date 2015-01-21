0ff30779477bae3cf2da808d09791f1ba5aa8727
Ben Hamner <ben@benhamner.com>
2015-01-21 09:18:40 -0600

    compare RF and BART importances

--- experiments/tic_tac_toe/param_importances.jl
+++ experiments/tic_tac_toe/param_importances.jl
@@ -7,4 +7,5 @@ d = d[d[:Opponent].=="rand",:]
 delete!(d, :LossPercentage)
 delete!(d, :DrawPercentage)
 println(d)
-draw(PNG("plots/randomize_parameters_importances.png", 8inch, 6inch), plot(importances(d, :WinPercentage, regression_forest_options())))
+draw(PNG("plots/randomize_parameters_importances_rf.png", 8inch, 6inch), plot(importances(d, :WinPercentage, regression_forest_options())))
+draw(PNG("plots/randomize_parameters_importances_bart.png", 8inch, 6inch), plot(importances(d, :WinPercentage, bart_options())))
