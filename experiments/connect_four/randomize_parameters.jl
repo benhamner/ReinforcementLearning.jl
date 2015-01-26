d11ef6263bc39aff86891729990efc647e65e398
Ben Hamner <ben@benhamner.com>
2015-01-25 19:54:50 -0800

    added connect four version of randomize parameters

---
+++ experiments/connect_four/randomize_parameters.jl
@@ -0,0 +1,63 @@
+using Gadfly
+using MachineLearning
+using ReinforcementLearning
+
+num_trials = 100_000
+
+hidden_layers  = Vector{Int}[[5], [10], [25], [50], [100], [200], [10,10], [25,25], [25,10], [50,50], [100,10]]
+learning_rates = vcat([[1,2,5].*10.0^x for x=-5:5]...)
+num_games      = [1_000, 2_000, 5_000, 10_000]
+alphas         = [0.05:0.05:1.0]
+num_test_games = 200
+regularization_factors = vcat(0.0, vcat([[1,2,5].*10.0^x for x=-3:0]...))
+
+evaluation_opponents = [random_player, make_lookahead_player(1), make_lookahead_player(2)]
+evaluation_opponent_names = ["rand", "lh1", "lh2"]
+
+res = DataFrame(Opponent=[],
+                HiddenLayer=[],
+                LearningRates=[],
+                RegularizationFactor=[],
+                NumGames=[],
+                Alpha=[],
+                Trial=[],
+                WinPercentage=[],
+                DrawPercentage=[],
+                LossPercentage=[])
+
+for i_trial=1:num_trials
+    println("Trial ", i_trial)
+    layers        = rand(hidden_layers)
+    learning_rate = rand(learning_rates)
+    games         = rand(num_games)
+    alpha         = rand(alphas)
+    regularization_factor = rand(regularization_factors)
+    println("--Layers: ", layers, ", LRate: ", learning_rate, "\n--Games: ", games, ", Alpha: ", alpha)
+
+    q, q_player = train_q_net_player(play_connect_four_track_state,
+                                     84,
+                                     [random_player],
+                                     num_games=games,
+                                     net_options=regression_net_options(hidden_layers=layers,
+                                                                        regularization_factor=regularization_factor,
+                                                                        learning_rate=learning_rate),
+                                     self_play=true,
+                                     alpha = alpha)
+    for i_opponent = 1:length(evaluation_opponents)
+        win_percentage, draw_percentage, loss_percentage, results_txt = evaluate_connect_four_players(q_player, evaluation_opponents[i_opponent], num_test_games)
+        opponent_name = evaluation_opponent_names[i_opponent]
+        res = vcat(res, DataFrame(Opponent=[opponent_name],
+                                  HiddenLayer=Vector{Int}[layers],
+                                  LearningRates=[learning_rate],
+                                  RegularizationFactor=[regularization_factor],
+                                  NumGames=[games],
+                                  Alpha=[alpha],
+                                  Trial=[i_trial],
+                                  WinPercentage=[win_percentage],
+                                  DrawPercentage=[draw_percentage],
+                                  LossPercentage=[loss_percentage]))
+    end
+    writetable("plots/randomize_parameters.csv", res)
+end
+
+println(res)
